d6e63194690c284b82b3c6a54473c8e682a72b58
Hari Kammana <hari.kammana@gmail.com>
2022-07-28 18:47:58 +0530

    initial commit

---
+++ examples/jenkins-ha/.terraform.lock.hcl
@@ -0,0 +1,9 @@
+# This file is maintained automatically by "terraform init".
+# Manual edits may be lost in future updates.
+
+provider "registry.terraform.io/hashicorp/aws" {
+  version = "4.14.0"
+  hashes = [
+    "h1:wJ1F5KqM9XLQqotZ82YFQywpN4pwtVFR35uc5ckqGKw=",
+  ]
+}
---
+++ examples/jenkins-ha/variables.tf
@@ -0,0 +1,15 @@
+variable "region" {
+  default = "us-east-1"
+}
+
+variable "subnet_ids" {
+  default = ["subnet-0d0ee9af02f44ded3", "subnet-05c63a6ab0a59de20"]
+}
+
+variable "vpc_id" {
+  default = "vpc-0e189db93e233fdc7"
+}
+
+variable "app_name" {
+  default = "jenkins"
+}
